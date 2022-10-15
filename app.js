06c35efdc697d3e0b8aff2abd6f7e9e1323fb74f
devsamahd <devsamahd@gmail.com>
2022-10-15 12:27:43 +0100

    stated creating the API, major branch

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-console.log('some changes')
-//testing
+const {format} = require('date-fns')
+const {v4: uuid} = require('uuid')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+const path = require('path')
+const logEvents = require('./logEvents')
+const EventEmmitter = require('events')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 300
+
+
+app.use(express.static(__dirname + '/public'))
+app.get('/docs', (req,res)=>{
+    res.sendFile(path.join(__dirname,'views', 'documentation.html'))
+})
+
+app.listen(PORT, ()=>{
+    console.log(`listening on port ${PORT}`)
+})
+
+class MyEmmitter extends EventEmmitter{}
+
+const myEmmitter = new MyEmmitter()
+
+myEmmitter.on('log', (msg) => logEvents(msg))
+
+process.on('uncaughtException', err =>{
+    myEmmitter.emit('log', err)
+})
+//     
+// },2000)
